0bb018ad4273f97ae747e0bbc865ad492d916dc0
katsuokaisao <isaoda0@icloud.com>
2023-12-08 17:14:51 +0900

    ad server

---
+++ src/go/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	pb "github.com/katsuokaisao/proto-gen-sample/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	port := 8080
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+
+	pb.RegisterPinPonServiceServer(s, NewPinPonSever())
+
+	reflection.Register(s)
+
+	go func() {
+		log.Printf("start gRPC server port: %v", port)
+		s.Serve(listener)
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("stopping gRPC server...")
+	s.GracefulStop()
+}
+
+func NewPinPonSever() *PinPonSever {
+	return &PinPonSever{}
+}
+
+type PinPonSever struct {
+	pb.UnimplementedPinPonServiceServer
+}
+
+func (s *PinPonSever) Send(ctx context.Context, req *pb.PinPonRequest) (*pb.PinPonResponse, error) {
+	fmt.Printf("receive %s\n", req.GetWord())
+	return &pb.PinPonResponse{Word: "Pon"}, nil
+}
